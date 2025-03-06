c05d29dd9eace750f8e5978684753de3892ec517
Emrane23 <omranklaai@gmail.com>
2025-03-06 14:05:52 +0100

    set up db

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Customer;
+use App\Models\Order;
+use App\Models\Product;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +22,29 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        $products = Product::factory(20)->create();
+
+        Customer::factory(10)->create()->each(function ($customer) use ($products) {
+            Order::factory(rand(1, 3))->create(['customer_id' => $customer->id])->each(function ($order) use ($products) {
+                $total = 0;
+
+                $selectedProducts = $products->random(rand(1, 5));
+
+                foreach ($selectedProducts as $product) {
+                    $quantity = rand(1, 5);
+                    $price = $product->price * $quantity;
+
+                    $order->products()->attach($product->id, [
+                        'quantity' => $quantity,
+                        'price' => $price,
+                    ]);
+
+                    $total += $price;
+                }
+
+                $order->update(['total' => $total]);
+            });
+        });
     }
 }
