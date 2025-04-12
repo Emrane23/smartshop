315d3d0dbc97c8fd7c41f5a90ba70f74d3455828
Emrane23 <omranklaai@gmail.com>
2025-04-12 18:24:31 +0200

    fix bug name admin in navbar

--- resources/views/dashboard/partials/navbar.blade.php
+++ resources/views/dashboard/partials/navbar.blade.php
@@ -13,21 +13,17 @@
             <li class="nav-item dropdown user-menu"> <a href="#" class="nav-link dropdown-toggle"
                     data-bs-toggle="dropdown"> <img src="{{ asset('assets/img/avatar.png') }}"
                         class="user-image rounded-circle shadow" alt="User Image"> <span
-                        class="d-none d-md-inline">Alexander Pierce</span> </a>
+                        class="d-none d-md-inline">{{ auth()->user()->name }}</span> </a>
                 <ul class="dropdown-menu dropdown-menu-lg dropdown-menu-end"> <!--begin::User Image-->
                     <li class="user-header text-bg-primary"> <img src="{{ asset('assets/img/avatar.png') }}"
                             class="rounded-circle shadow" alt="User Image">
                         <p>
-                            Alexander Pierce - Web Developer
-                            <small>Member since Nov. 2023</small>
+                            {{ auth()->user()->name }}
+                            <small>admin since  {{ auth()->user()->created_at->format('M. Y') }}</small>
                         </p>
                     </li> <!--end::User Image--> <!--begin::Menu Body-->
                     <li class="user-body"> <!--begin::Row-->
-                        <div class="row">
-                            <div class="col-4 text-center"> <a href="#">Followers</a> </div>
-                            <div class="col-4 text-center"> <a href="#">Sales</a> </div>
-                            <div class="col-4 text-center"> <a href="#">Friends</a> </div>
-                        </div> <!--end::Row-->
+                        
                     </li> <!--end::Menu Body--> <!--begin::Menu Footer-->
                     <li class="user-footer">
                         <a href="#" class="btn btn-default btn-flat">Profile</a>
